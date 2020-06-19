4892e4424ad3f580feffc78e06a2af32b3acade4
James <james.hudgins.work@gmail.com>
2020-06-18 21:02:09 -0400

    got things to display to the DOM

--- javascript.js
+++ javascript.js
@@ -1,23 +1,56 @@
-$("button").on("click", function() {
+$(".button").on("click", function() {
+    event.preventDefault()
+    var search = $('#search').val()
+    var numberOf = $("#numberOf").val()
+    console.log(search)
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=fUAV70oYpTeS4X52k9Ce031QOrQOi4Tg";
+    console.log(queryURL)
     
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
+
+    
+ 
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-
-
+        console.log(response.response)
+        
+    // first clear out the results container
+    $("#results").empty();
+    for (i = 0; i < numberOf; i++) {
+        // get current article title
+        var articleTitle = response.response.docs[i].abstract
+        console.log(articleTitle)
+        // create a new div
+        var newDiv = $("<div>")
+        // add a para for the article name
+        var newPara = $("<p>")
+        // name the paragraph with the current doc abstract
+        newPara.text(articleTitle)
+        newPara.appendTo(newDiv);
+        newDiv.appendTo("#results");
+        
+    }    
+    
 
     });
-  });
 
-  $.ajax({
-  url: queryURL,
-  method: 'GET'
-  }).then(function(response){
-  
-  });
+
+    // Headline
+    // var headline = response.?
+    // Body
+    // var body = response.?
+
+    
+
+
+
+
+});
+
+// Alex, here are the ids
+
+// numberOf
 
 //   KEY fUAV70oYpTeS4X52k9Ce031QOrQOi4Tg
